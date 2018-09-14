f12b5b63a11b9d0cf1441af1837274fb6bce42a3
gokatz <gokulkathirvel@live.com>
2018-09-14 17:55:16 +0530

    adds initail commit for webpacking an extension

---
+++ utils/constant.js
@@ -0,0 +1,14 @@
+// user option messages
+const buildOptionMsg = {
+  raw: 'No Build tool. (recommended for small scale extension)',
+  webpack: 'Webpack. (recommended for a extensive extension)',
+  popup: 'Need a popup page when clicking on extension icon',
+  bg: 'Has background activity',
+  content: 'Need to manipulate other website with your chrome extension',
+  jquery: 'Need jQuery for DOM maniuplation',
+  bs: 'Need Bootstrap for basic styling'
+};
+
+module.exports = {
+  buildOptionMsg
+}
